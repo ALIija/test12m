ff131f4f4c7e4c4bc3d93568277219afef033358
Alija <vasijevaalija@gmail.com>
2022-05-01 23:57:21 +0300

    commit

--- src/Logs.java
+++ src/Logs.java
@@ -6,14 +6,14 @@ public class Logs {
 
     public static void main(String[] args) throws Exception {
      
-        ImageIcon image = new ImageIcon("load.jpg");
+        ImageIcon image = new ImageIcon("src//loader_gif_1.gif");
 
         JLabel label = new JLabel();
         label.setIcon(image);
 
         JFrame f = new JFrame();
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setSize(500,500);
+        f.setSize(800,500);
         f.setVisible(true);
         f.add(label);
 }
--- src/Veikals.java
+++ src/Veikals.java
@@ -1,7 +1,24 @@
+import java.io.ObjectInputStream.GetField;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 public class Veikals {
     public static void main(String[] args) throws Exception {
         IDparoles idparoles = new IDparoles();
 
         LoginLapa loginLapa = new LoginLapa(idparoles.getLogininfo());
+
+        ImageIcon image = new ImageIcon("src//loader_gif_1.gif");
+
+        JLabel label = new JLabel();
+        label.setIcon(image);
+
+        JFrame f = new JFrame();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(800,500);
+        f.setVisible(true);
+        f.add(label);
     }
 }
