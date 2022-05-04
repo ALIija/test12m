299e404be2855303e61465744d33dd3ceb87e164
Alija <vasijevaalija@gmail.com>
2022-05-04 21:31:06 +0300

    commit

--- src/Veikals.java
+++ src/Veikals.java
@@ -1,6 +1,7 @@
 import java.io.ObjectInputStream.GetField;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
@@ -10,16 +11,24 @@ public class Veikals {
 
         LoginLapa loginLapa = new LoginLapa(idparoles.getLogininfo());
 
+
         
         ImageIcon image = new ImageIcon("src//loader_gif_1.gif");
 
         JLabel label = new JLabel();
         label.setIcon(image);
+        // JButton OkButton = new JButton("Okey");
+        // OkButton.setBounds(125, 200, 100, 25);
+        // // OkButton.setFocusable(false);
+        // // OkButton.addActionListener(this);
 
         JFrame f = new JFrame();
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         f.setSize(800,500);
         f.setVisible(true);
         f.add(label);
+        // f.add(OkButton);
+        
     }
 }
