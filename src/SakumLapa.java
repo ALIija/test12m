98d26518beddfea7c772cc6381a4a40d65cd481b
Alija <vasijevaalija@gmail.com>
2022-05-09 21:00:14 +0300

    commit

--- src/SakumLapa.java
+++ src/SakumLapa.java
@@ -13,38 +13,38 @@ public class SakumLapa {
 
     SakumLapa(String userID) {
         JFrame frame = new JFrame("Sākumlapa");
-        JMenuBar mb = new JMenuBar();
+        // JMenuBar mb = new JMenuBar();
 
-        menu = new JMenu("Darbinieki");
-        submenu = new JMenu("visi");
-        i1 = new JMenuItem("Jaunie 1");
-        i2 = new JMenuItem("Esošie 2");
-        menu1 = new JMenu("Produkti");
-        submenu1 = new JMenu("Sadzīves tehnika");
-        k1 = new JMenuItem("Augļi");
-        k2 = new JMenuItem("Dārzeņi");
-        k3 = new JMenuItem("Gaļa");
-        k4 = new JMenuItem("Zivis");
-        k5 = new JMenuItem("Panna");
-        k6 = new JMenuItem("Sadzīves ķīmija");
+        // menu = new JMenu("Darbinieki");
+        // submenu = new JMenu("visi");
+        // i1 = new JMenuItem("Jaunie 1");
+        // i2 = new JMenuItem("Esošie 2");
+        // menu1 = new JMenu("Produkti");
+        // submenu1 = new JMenu("Sadzīves tehnika");
+        // k1 = new JMenuItem("Augļi");
+        // k2 = new JMenuItem("Dārzeņi");
+        // k3 = new JMenuItem("Gaļa");
+        // k4 = new JMenuItem("Zivis");
+        // k5 = new JMenuItem("Panna");
+        // k6 = new JMenuItem("Sadzīves ķīmija");
 
 
-        menu.add(i1);
-        menu.add(i2);
-        menu.add(submenu);
-        mb.add(menu);
-        frame.setJMenuBar(mb);
-        menu1.add(k1);
-        menu1.add(k2);
-        menu1.add(k3);
-        menu1.add(k4);
-        menu1.add(k5);
-        menu1.add(k5);
-        submenu1.add(k5);
-        menu1.add(submenu1);
-        mb.add(menu1);
+        // menu.add(i1);
+        // menu.add(i2);
+        // menu.add(submenu);
+        // mb.add(menu);
+        // frame.setJMenuBar(mb);
+        // menu1.add(k1);
+        // menu1.add(k2);
+        // menu1.add(k3);
+        // menu1.add(k4);
+        // menu1.add(k5);
+        // menu1.add(k5);
+        // submenu1.add(k5);
+        // menu1.add(submenu1);
+        // mb.add(menu1);
         
-        frame.setJMenuBar(mb);
+        // frame.setJMenuBar(mb);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(420, 420);
         frame.setLayout(null);
