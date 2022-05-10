bb260201e3e6dbe20ea1b9ffde84bd94faabec42
Alija <vasijevaalija@gmail.com>
2022-05-10 11:47:28 +0300

    commit

--- src/Veikals.java
+++ src/Veikals.java
@@ -1,24 +1,19 @@
 
-
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class Veikals {
     public static void main(String[] args) throws Exception {
-        IDparoles idparoles = new IDparoles();
-        
-
-        LoginLapa loginLapa = new LoginLapa(idparoles.getLogininfo());
+       
+        GFG idparoles = new GFG();
+        LoginLapa loginLapa = new LoginLapa(idparoles.HashMapFromTextFile());
 
         ImageIcon image = new ImageIcon("src//loader_gif_1.gif");
 
         JLabel label = new JLabel();
         label.setIcon(image);
-        // JButton OkButton = new JButton("Okey");
-        // OkButton.setBounds(125, 200, 100, 25);
-        // // OkButton.setFocusable(false);
-        // // OkButton.addActionListener(this);
+      
 
         JFrame f = new JFrame();
 
@@ -27,7 +22,7 @@ public class Veikals {
         f.setVisible(true);
         f.add(label);
         f.setLocation(400, 400);
-        // f.add(OkButton);
+        
 
     }
 }
